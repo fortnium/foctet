a3c9d5b90b7fff669a0f794934c35da219c5ed52
shellrow <shellrow@fortnium.com>
2024-11-05 00:28:25 +0900

    Add options

--- foctet-cli/src/handler/receive.rs
+++ foctet-cli/src/handler/receive.rs
@@ -1,6 +1,22 @@
 use clap::ArgMatches;
 use anyhow::Result;
 
-pub fn handle(_args: &ArgMatches) -> Result<()> {
+pub fn handle(args: &ArgMatches) -> Result<()> {
+    let sub_args = match args.subcommand_matches("receive") {
+        Some(matches) => matches,
+        None => {
+            tracing::error!("Failed to get subcommand matches");
+            anyhow::bail!("Failed to get subcommand matches");
+        },
+    };
+    match sub_args.get_one::<String>("ticket") {
+        Some(ticket) => {
+            tracing::info!("Ticket: {}", ticket);
+        },
+        None => {
+            tracing::error!("Failed to get ticket");
+            anyhow::bail!("Failed to get ticket");
+        }
+    }
     Ok(())
 }
--- foctet-cli/src/handler/send.rs
+++ foctet-cli/src/handler/send.rs
@@ -1,6 +1,23 @@
 use clap::ArgMatches;
 use anyhow::Result;
 
-pub fn handle(_args: &ArgMatches) -> Result<()> {
+pub fn handle(args: &ArgMatches) -> Result<()> {
+    let sub_args = match args.subcommand_matches("send") {
+        Some(matches) => matches,
+        None => {
+            tracing::error!("Failed to get subcommand matches");
+            anyhow::bail!("Failed to get subcommand matches");
+        },
+    };
+    if sub_args.contains_id("file") {
+
+    } else if sub_args.contains_id("dir") {
+
+    } else if sub_args.contains_id("text") {
+
+    } else {
+        tracing::error!("No file, directory, or text specified");
+        anyhow::bail!("No file, directory, or text specified");
+    }
     Ok(())
 }
